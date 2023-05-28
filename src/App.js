356a8146e89663c3bfb62c2ab26a2a18b4e9a858
Ajay <ajayputta5871@gmail.com>
2023-05-28 23:37:10 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import Home from './Home'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 const App = () => {
   return (
-    <div>
-      <h1>Ajay putta</h1>
+    <div className='App'>
+      <Router>
+<Routes>
+< Route exact path="/" element={<Home />} />
+{/* <Route exact path="/employe" element={<EmployeeList />} /> */}
+{/* <Route exact path="/AddEmployee" element={<AddEmployee />} /> */}
+{/* <Route exact path="/editEmployee" element={<EditEmployee />} /> */}
+</Routes>
+</Router>
     </div>
   )
 }
